916f1b76b4cb16c150b93385907252cacc74bd41
Markus Jarvis <markus.h.jarvis@gmail.com>
2022-06-06 21:21:44 -0500

    Add 2 more solutions

---
+++ 8kyu_solutions/Century_from_Year_Solution.js
@@ -0,0 +1,3 @@
+function century(year) {
+    return Math.ceil(year/100);   //Math.ceil rounds up to next largest integer
+  }
---
+++ 8kyu_solutions/Keep_Hydrated!.js
@@ -0,0 +1,4 @@
+function litres(time) {
+    let amount = Math.floor(time * 0.5);
+    return amount;
+  }
