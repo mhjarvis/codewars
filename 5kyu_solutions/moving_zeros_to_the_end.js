829a1539d68b494d3e8c873cdf0103bc7a8dbae3
Markus Jarvis <markus.h.jarvis@gmail.com>
2022-06-18 03:54:10 -0500

    arrays, push()

---
+++ 5kyu_solutions/moving_zeros_to_the_end.js
@@ -0,0 +1,15 @@
+function moveZeros(arr) {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+      if(arr[i] !== 0) {
+        newArr.push(arr[i]);
+      }
+    }
+    for(let i = 0; i < arr.length; i++) {
+      if(arr[i] === 0) {
+        newArr.push(arr[i]);
+      }
+    }
+    console.log(newArr);
+    return newArr;
+  }
