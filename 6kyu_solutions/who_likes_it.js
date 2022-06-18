b0f64e789af0d7eb8db47509623d447733088d1f
Markus Jarvis <markus.h.jarvis@gmail.com>
2022-06-18 03:46:55 -0500

    arrays, length();

---
+++ 6kyu_solutions/who_likes_it.js
@@ -0,0 +1,19 @@
+function likes(names) {
+    let length = names.length;
+    
+    if(length == 0) {
+      return 'no one likes this';
+    };
+    if(length == 1) {
+      return `${names[0]} likes this`;
+    }
+    if(length == 2) {
+      return `${names[0]} and ${names[1]} like this`;
+    }
+    if(length == 3) {
+       return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    }
+    if(length => 4) {
+       return `${names[0]}, ${names[1]} and ${length - 2} others like this`;
+    }
+  }
