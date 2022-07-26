756c2e4c179069a5b541fad56ee7b02af2617141
Markus <markus.h.jarvis@gmail.com>
2022-07-26 13:17:34 -0900

    add solution

---
+++ 8kyu_solutions/part_3/quarter_of_the_year.js
@@ -0,0 +1,13 @@
+const quarterOf = (month) => {
+
+    if(month <= 3) {
+      return 1;
+    }
+    if(month <= 6) {
+      return 2;
+    }
+    if(month <= 9) {
+      return 3;
+    }
+    return 4;  
+  }
