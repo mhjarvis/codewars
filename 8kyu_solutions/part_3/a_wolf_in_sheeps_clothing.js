44a09c8cd6fa2ae60ab87675e5074d96fedced51
Markus <markus.h.jarvis@gmail.com>
2022-07-31 23:43:20 -0700

    Update a_wolf_in_sheeps_clothing.js

--- 8kyu_solutions/part_3/a_wolf_in_sheeps_clothing.js
+++ 8kyu_solutions/part_3/a_wolf_in_sheeps_clothing.js
@@ -1,8 +1,10 @@
 function warnTheSheep(queue) {
+  
   let n = 0;
   if(queue[queue.length - 1] == 'wolf') {
     return 'Pls go away and stop eating my sheep'
   }
+  
   for(let i = queue.length - 1; i >= 0; i--) {
     if(queue[i] == 'wolf') {
       return 'Oi! Sheep number ' + n + '! You are about to be eaten by a wolf!';
