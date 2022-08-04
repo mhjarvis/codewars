da2ae656c13fe6fc3e19ba08b418a08a981b9efb
Markus <markus.h.jarvis@gmail.com>
2022-08-03 22:36:26 -0700

    Add find_the_parity_outlier.js and complementary_dna.js solutions

---
+++ 6kyu_solutions/find_the_parity_outlier.js
@@ -0,0 +1,24 @@
+function findOutlier(integers){
+    let firstOdd = 0;
+    let firstEven = 0;
+    let oddCount = 0;
+    let evenCount = 0;
+    console.log(-10 % 2);
+    
+    for(let i = 0; i < integers.length; i++) {
+      if(integers[i] % 2 == 0 || integers[i] % 2 == -0) {
+        firstEven = integers[i];
+        evenCount++;
+        continue;
+      }
+      if(integers[i] % 2 == 1 || integers[i] % 2 == -1) {
+        firstOdd = integers[i];
+        oddCount++;
+        continue;
+      }
+    }
+    if(oddCount == 1) {
+      return firstOdd;
+    }
+    return firstEven;
+  }
