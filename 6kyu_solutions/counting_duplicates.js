b9b15ebf1a1e8db27e0603bf447df27387abfcfb
Markus <markus.h.jarvis@gmail.com>
2022-08-04 23:01:42 -0700

    Add notes to code

--- 6kyu_solutions/counting_duplicates.js
+++ 6kyu_solutions/counting_duplicates.js
@@ -1,15 +1,15 @@
 function duplicateCount(text){
-    let count = 0;
-    text = text.toLowerCase().split("");
-    let arr = text;
+    let count = 0;      //maintain number of duplicates
+    text = text.toLowerCase().split("");        //standardize for case-sensitivity
+    let arr = text;     
     
     while(arr.length > 0) {
-      let temp = arr[0];
-      let tempArr = arr.filter(word => word == temp);
-      if(tempArr.length > 1) {
-        count++;
+      let temp = arr[0];        //assign first element of arr to temp
+      let tempArr = arr.filter(word => word == temp);       //create temp arr with all values of temp
+      if(tempArr.length > 1) {      //if arr has more than one value
+        count++;                    //increase count
       }
-      arr = arr.filter(word => word != temp);
+      arr = arr.filter(word => word != temp);       //reset arr without temp value
     }
     return count;
   }
