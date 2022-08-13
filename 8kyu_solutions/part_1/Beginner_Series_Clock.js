6675017882589e4c53324f91de3ae8c44b2c411f
Markus <markus.h.jarvis@gmail.com>
2022-08-13 18:15:33 -0500

    Remove unnecessary comments and spacing

--- 8kyu_solutions/part_1/Beginner_Series_Clock.js
+++ 8kyu_solutions/part_1/Beginner_Series_Clock.js
@@ -1,21 +1,13 @@
 function past(h, m, s){
 
-    let total = 0;    //variable holds total seconds
+    let total = 0;
     
-    //add hours converted to seconds to total
     if(h > 0) {
       total = h * 3600;
     }
-    
-    //add minutes converted to seconds to total
     if(m > 0) {
       total += (m * 60);
     }
-    
-    //add remaining seconds to total
     total += s;
-    
-    //convert total seconds to miliseconds
     return total * 1000;
-  
   }
