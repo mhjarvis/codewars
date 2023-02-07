4e7b6889ed9b0c729883f2afbc0e8c70a66eb8e5
Markus <markus.h.jarvis@gmail.com>
2023-02-07 16:30:46 -0600

    Add intro for problem set 2

--- random/main.js
+++ random/main.js
@@ -14,6 +14,27 @@ function sumRange(n) {
 
   return n + sumRange(n - 1);
 }
-
+console.log('Proplem 1:')
 console.log('The sum of the range of numbers for 8 is ' + sumRange(8));
 
+
+/* POWER FUNCTION
+ * 
+ * Write a function called power which takes in a base and an exponent. If the
+ * exponent is 0, return 1.
+ * 
+ * Sample: console.log(power(2, 4)); // 16
+ */
+
+function power(x, y) {
+  
+  if(y == 0) {
+    return 1;
+  }
+
+  return x + x + power(x, y - 1);
+
+}
+
+console.log('Problem 2:');
+console.log('The power of 3 to the 4th power is: ' + power(3, 4));
