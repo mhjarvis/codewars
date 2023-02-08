fbc935a49ee70d89e5fcd91f235e56f168b0b248
Markus <markus.h.jarvis@gmail.com>
2023-02-08 15:27:49 -0600

    add solution 8

--- random/main.js
+++ random/main.js
@@ -190,3 +190,46 @@ function totalIntegers(array){
 
 console.log('\nProblem 7: ');
 console.log('Integer count: ' + seven);
+
+/* WRITE A FUNCTION THAT SUMS SQUARES OF NUMBERS IN LIST THAT MAY CONTAIN MORE LISTS
+ *
+ * Sample: 
+ * var l = [1,2,3]; 
+ * console.log(SumSquares(l)); // 1 + 4 + 9 = 14
+ *
+ * l = [[1,2],3]; 
+ * console.log(SumSquares(l)); // 1 + 4 + 9 = 14
+ * 
+ * l = [[[[[[[[[1]]]]]]]]] 
+ * console.log(SumSquares(l)); // 1 = 1
+ * 
+ * l = [10,[[10],10],[10]] 
+ * console.log(SumSquares(l)); // 100 + 100 + 100 + 100 = 400
+ */
+
+let l = [1,2,3]; 
+console.log(SumSquares(l)); // 14
+
+l = [[1,2],3]; 
+console.log(SumSquares(l)); // 14
+
+l = [[[[[[[[[1]]]]]]]]] 
+console.log(SumSquares(l)); // 1
+
+l = [10,[[10],10],[10]] 
+console.log(SumSquares(l)); // 400
+
+function SumSquares(array){
+	if(array.length === 0) return 0;
+	let total = 0;
+
+	for(let i = 0; i < array.length; i++){
+		if(Array.isArray(array[i])){
+			total += SumSquares(array[i]);
+		} else {
+			total += array[i] * array[i];
+		}
+		
+	}
+	return total;
+}
