d41d39065f5a35f7ffbde1e82b5829f7e9e44bbc
Markus <markus.h.jarvis@gmail.com>
2023-02-08 15:29:45 -0600

    Add question 9 solution

--- random/main.js
+++ random/main.js
@@ -233,3 +233,22 @@ function SumSquares(array){
 	}
 	return total;
 }
+
+/* The function should return an array containing repetitions of the number argument. For instance, 
+ * replicate(3, 5) should return [5,5,5]. If the times argument is negative, return an empty array.
+ * 
+ * Sample:
+ * console.log(replicate(3, 5)) // [5, 5, 5]
+ * console.log(replicate(1, 69)) // [69]
+ * console.log(replicate(-2, 6)) // []
+ */
+
+console.log(replicate(3, 5)) // [5, 5, 5]
+console.log(replicate(1, 69)) // [69]
+console.log(replicate(-2, 6)) // []
+
+function replicate(times, number){
+	if(times <= 0) return [];
+
+	return [number].concat(replicate(times - 1, number));
+}
